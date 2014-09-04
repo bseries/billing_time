b4b9e774b5c5c7e67f5a1ede465de5a697374b1a
David Persson <davidpersson@gmx.de>
2014-09-04 12:48:22 +0200

    Adding plans model.

---
+++ models/Plans.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Billing Recur
+ *
+ * Copyright (c) 2014 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+namespace billing_recur\models;
+
+class Plans extends \cms_core\models\Base {
+
+	protected $_meta = [
+		'source' => 'billing_plans'
+	];
+
+	protected static $_actsAs = [
+		'cms_core\extensions\data\behavior\Localizable' => [
+			'fields' => [
+				'amount' => 'money',
+				'quantity' => 'decimal'
+			]
+		]
+	];
+}
+
+?>
