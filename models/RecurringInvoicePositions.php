417c58f505a807fff1f38a457eccb549fd96cc06
David Persson <davidpersson@gmx.de>
2015-06-16 15:30:41 +0200

    Fix usage of ownable / user behavior.

--- models/RecurringInvoicePositions.php
+++ models/RecurringInvoicePositions.php
@@ -34,7 +34,7 @@ class RecurringInvoicePositions extends \base_core\models\Base {
 	];
 
 	protected static $_actsAs = [
-		'base_core\extensions\data\behavior\Ownable',
+		'base_core\extensions\data\behavior\User',
 		'base_core\extensions\data\behavior\Timestamp',
 		'base_core\extensions\data\behavior\Localizable' => [
 			'fields' => [
--- models/ScheduledInvoicePositions.php
+++ models/ScheduledInvoicePositions.php
@@ -33,7 +33,7 @@ class ScheduledInvoicePositions extends \base_core\models\Base {
 	];
 
 	protected static $_actsAs = [
-		'base_core\extensions\data\behavior\Ownable',
+		'base_core\extensions\data\behavior\User',
 		'base_core\extensions\data\behavior\Timestamp',
 		'base_core\extensions\data\behavior\Localizable' => [
 			'fields' => [
