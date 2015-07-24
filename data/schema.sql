81518310a8cb93dc8fcb7cdbe2fdf14336023db9
David Persson <davidpersson@gmx.de>
2015-07-24 13:37:50 +0200

    Update schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,12 +1,22 @@
--- Create syntax for TABLE 'billing_plans'
-CREATE TABLE `billing_plans` (
+-- Create syntax for TABLE 'billing_recurring_invoice_positions'
+CREATE TABLE `billing_recurring_invoice_positions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL DEFAULT '',
+  `user_id` int(11) unsigned DEFAULT NULL,
+  `virtual_user_id` int(11) unsigned DEFAULT NULL,
+  `description` varchar(250) NOT NULL,
+  `quantity` decimal(10,2) unsigned NOT NULL DEFAULT '1.00',
   `amount_currency` char(3) NOT NULL DEFAULT 'EUR',
   `amount_type` char(5) NOT NULL DEFAULT 'net',
+  `amount_rate` int(5) unsigned NOT NULL DEFAULT '0',
   `amount` int(10) NOT NULL,
+  `frequency` varchar(100) NOT NULL DEFAULT 'monthly',
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `runs` int(10) unsigned DEFAULT '0',
+  `ran` datetime DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='mirrors billing_invoice_positions';
 
 -- Create syntax for TABLE 'billing_scheduled_invoice_positions'
 CREATE TABLE `billing_scheduled_invoice_positions` (
@@ -17,14 +27,10 @@ CREATE TABLE `billing_scheduled_invoice_positions` (
   `quantity` decimal(10,2) unsigned NOT NULL DEFAULT '1.00',
   `amount_currency` char(3) NOT NULL DEFAULT 'EUR',
   `amount_type` char(5) NOT NULL DEFAULT 'net',
+  `amount_rate` int(5) unsigned NOT NULL DEFAULT '0',
   `amount` int(10) NOT NULL,
-  `start` date NOT NULL COMMENT 'start of billing period; for info',
-  `stop` date NOT NULL COMMENT 'stop of billing period; for info',
+  `run_on` datetime DEFAULT NULL COMMENT 'NULL to run immediately',
   `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='mirrors billing_invoice_posititions';
-
-ALTER TABLE `users` ADD `billing_invoice_frequency` VARCHAR(20) NOT NULL  DEFAULT 'monthly'  AFTER `shipping_address_id`;
-ALTER TABLE `users` ADD `billing_plan_id` INT(11)  UNSIGNED  NOT NULL  AFTER `billing_invoice_frequency`;
-
-
